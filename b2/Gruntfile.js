f7d7b841540c60f02694ed7c86a95260e1193d41
lostinflycity <724138287@qq.com>
2016-03-23 19:58:04 +0800

    增加js合并压缩
    顺便把b1的main.js 压缩了main.min.js 可以发布了

--- b2/Gruntfile.js
+++ b2/Gruntfile.js
@@ -1,57 +1,81 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    connect:{
-      //这里为插件子刷新方式
-      options: {
-        port: 9000,
-        hostname: 'localhost', //默认就是这个值，可配置为本机某个 IP，localhost 或域名
-        livereload: 35729  //声明给 watch 监听的端口
-      },
-      server: {
-        options: {
-          open: true, //自动打开网页 http://
-          base: [
-            '.'  //主目录
-            ]
-          }
-        }
-      
-    },
-    watch: {
-      // sass: {
-      //   files: ['sass/**/*.{scss,sass}','sass/_partials/**/*.{scss,sass}'],
-      //   tasks: ['compass:dist']
-      // },
-      livereload: {
-        options: {
-                  livereload:'<%=connect.options.livereload%>'  //监听前面声明的端口  35729
+module.exports = function(grunt)
+{
+    grunt.initConfig(
+    {
+        pkg: grunt.file.readJSON('package.json'),
+        concat: //合并文件
+        {
+            options:
+            { // 定义一个用于插入合并输出文件之间的字符
+                separator: ';'
+            },
+            dist:
+            { // 将要被合并的文件
+                src: ['app/javascripts/*.js'],
+                // 合并后的JS文件的存放位置
+                dest: 'app/js/<%= pkg.name %>.js'
+            }
+        },
+        uglify: //压缩
+        {
+            options:
+            { // 此处定义的banner注释将插入到输出文件的顶部
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist:
+            {
+                files:
+                { //uglify会自动压缩concat任务中生成的文件
+                    'app/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        connect:
+        {
+            //这里为插件子刷新方式
+            options:
+            {
+                port: 9000,
+                hostname: 'localhost', //默认就是这个值，可配置为本机某个 IP，localhost 或域名
+                livereload: 35729 //声明给 watch 监听的端口
+            },
+            server:
+            {
+                options:
+                {
+                    open: true, //自动打开网页 http://
+                    base: [
+                        '.' //主目录
+                    ]
+                }
+            }
+
+        },
+        watch:
+        {
+            livereload:
+            {
+                options:
+                {
+                    livereload: '<%=connect.options.livereload%>' //监听前面声明的端口  35729
                 },
-                files:[  //下面文件的改变就会实时刷新网页
-                'app/*.html',
-              'app/stylesheets/{,*/}*.css',
-            'app/javascripts/{,*/}*.js',
-          'app/images/{,*/}*.{png,jpg}'
+                files: [ //下面文件的改变就会实时刷新网页
+                    'app/*.html',
+                    'app/stylesheets/{,*/}*.css',
+                    'app/javascripts/{,*/}*.js',
+                    'app/images/{,*/}*.{png,jpg}'
 
-          ]
-        }
-      },
-
-    //   compass: {
-    //    dist: {
-    //     options: {
-    //       config: 'config.rb'
-    //     }
-    //   }
-    // }
+                ]
+            },
+            files: ['app/javascripts/**/*.js'],
+            tasks: ['concat', 'uglify']
+        },
+    });
 
 
-  });
-
-
-// grunt.loadNpmTasks('grunt-contrib-compass');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-connect');
-// grunt.registerTask('default', ['compass:dist','connect:server', 'watch']);
-grunt.registerTask('default', ['connect:server', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['connect:server', 'watch','concat', 'uglify']);
 };
